0c74c5cd3c39b50b5a4750f12f9a8d26a5e9bc2c
Tusharrajput007 <trajput@saffronsynaptiqsoftwarelab.com>
2025-07-01 15:57:53 +0530

    custom-component ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Login } from './login/login';
 import { Signup } from './signup/signup';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Login, Signup],
+  imports: [Login, Signup, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/profile/profile.component.html
@@ -0,0 +1,1 @@
+<h1>Profile component</h1>
