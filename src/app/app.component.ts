f51f0e5923586f4d094d737f3db9d905878be5fd
Tusharrajput007 <trajput@saffronsynaptiqsoftwarelab.com>
2025-07-01 16:29:38 +0530

    typescript updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,36 @@ import { Login } from './login/login';
 import { Signup } from './signup/signup';
 import { ProfileComponent } from './profile/profile.component';
 
-
 @Component({
   selector: 'app-root',
   imports: [Login, Signup, ProfileComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   protected title = 'angular-tut';
 
-  handleClickEvent(){
-    console.log("Fuction Called");
+  handleClickEvent() {
+    console.log('Fuction Called');
     this.otherFuction();
   }
 
-  otherFuction(){
-    console.log("Other fn Called");
+  otherFuction() {
+    console.log('Other fn Called');
+  }
+
+  name = 'Tushar';
+  data: string | number = 'Hello';
+  other: any = true;
+
+  updateName() {
+    this.name = 'Sidhu';
+    this.data = 12;
+    this.other = 'Anil';
+    this.other = 12;
+  }
+
+  handleSum(a:number,b:number){
+    console.log(a+b);
   }
 }
