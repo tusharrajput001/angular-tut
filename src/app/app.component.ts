170eb5a1a319d92f2c84729c8d05c8437ca8d103
Tusharrajput007 <trajput@saffronsynaptiqsoftwarelab.com>
2025-07-01 15:47:23 +0530

    signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Login } from './login/login';
+import { Signup } from './signup/signup';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Login],
+  imports: [Login, Signup],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   imports: [],
   templateUrl: './login.html',
   styleUrl: './login.css'
