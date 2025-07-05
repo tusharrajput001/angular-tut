07c310343ec0e60c19be692cd51ed50be8d1edbc
Tusharrajput007 <trajput@saffronsynaptiqsoftwarelab.com>
2025-07-05 14:03:18 +0530

    Add computed signals and update functionality in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { Login } from './login/login';
 import { Signup } from './signup/signup';
 import { ProfileComponent } from './profile/profile.component';
@@ -10,104 +10,133 @@ import { ProfileComponent } from './profile/profile.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  cnt = signal(10);
-  x:number = 20;
 
-  constructor() {
-    effect(() => {
-      console.log(this.cnt());
-      console.log(this.x);
-    });
+  x=signal(10);
+  y=signal(20);
+  z=computed(()=>this.x() + this.y()); 
+
+  showValue(){
+    console.log(this.z());
+    this.x.set(100)
+    console.log(this.z());
   }
 
-  updateVal() {
-    this.cnt.set(this.cnt() + 1);
+
+
+
+
+  data = signal< number | string >(10);
+
+  updateSignal(){
+    this.data.set("Hello");
   }
 
-  updateX() {
-    this.x = this.x + 1;
-  }
 
-  users = ['Tushar', 'ujjwal', 'Kunal', 'Ayush', 'Mayank'];
-  students = [
-    { name: 'Tushar', age: 23, address: 'Delhi' },
-    { name: 'ujjwal', age: 23, address: 'UP' },
-    { name: 'Kunal', age: 23, address: 'Delhi' },
-    { name: 'Ayush', age: 23, address: 'UP' },
-    { name: 'Mayank', age: 23, address: 'UP' },
-  ];
 
-  color = 1;
 
-  display = true;
 
-  handleColor(val: number) {
-    this.color = val;
-  }
+//   cnt = signal(10);
+//   x:number = 20;
 
-  handleInput(event: Event) {
-    this.color = parseInt((event.target as HTMLInputElement).value);
-  }
+//   constructor() {
+//     effect(() => {
+//       console.log(this.cnt());
+//       console.log(this.x);
+//     });
+//   }
 
-  name: string = '';
-  displayName = '';
+//   updateVal() {
+//     this.cnt.set(this.cnt() + 1);
+//   }
 
-  getName(event: Event) {
-    const name = (event.target as HTMLInputElement).value;
-    this.name = name;
-  }
+//   updateX() {
+//     this.x = this.x + 1;
+//   }
 
-  showName() {
-    this.displayName = this.name;
-  }
+//   users = ['Tushar', 'ujjwal', 'Kunal', 'Ayush', 'Mayank'];
+//   students = [
+//     { name: 'Tushar', age: 23, address: 'Delhi' },
+//     { name: 'ujjwal', age: 23, address: 'UP' },
+//     { name: 'Kunal', age: 23, address: 'Delhi' },
+//     { name: 'Ayush', age: 23, address: 'UP' },
+//     { name: 'Mayank', age: 23, address: 'UP' },
+//   ];
 
-  setName() {
-    this.name = 'Tushar';
-  }
+//   color = 1;
 
-  toogleDiv() {
-    this.display = !this.display;
-  }
+//   display = true;
 
-  count: number = 0;
+//   handleColor(val: number) {
+//     this.color = val;
+//   }
 
-  handleCounter(val: string) {
-    if (val == 'minus') {
-      if (this.count <= 1) {
-        this.count = 0;
-      } else {
-        this.count = this.count - 1;
-      }
-    } else if (val == 'plus') {
-      this.count = this.count + 1;
-    } else {
-      this.count = 0;
-    }
-  }
+//   handleInput(event: Event) {
+//     this.color = parseInt((event.target as HTMLInputElement).value);
+//   }
 
-  protected title = 'angular-tut';
+//   name: string = '';
+//   displayName = '';
 
-  handleClickEvent() {
-    console.log('Fuction Called');
-    this.otherFuction();
-  }
+//   getName(event: Event) {
+//     const name = (event.target as HTMLInputElement).value;
+//     this.name = name;
+//   }
 
-  otherFuction() {
-    console.log('Other fn Called');
-  }
+//   showName() {
+//     this.displayName = this.name;
+//   }
 
-  // name = 'Tushar';
-  data: string | number = 'Hello';
-  other: any = true;
+//   setName() {
+//     this.name = 'Tushar';
+//   }
 
-  updateName() {
-    this.name = 'Sidhu';
-    this.data = 12;
-    this.other = 'Anil';
-    this.other = 12;
-  }
+//   toogleDiv() {
+//     this.display = !this.display;
+//   }
 
-  handleSum(a: number, b: number) {
-    console.log(a + b);
-  }
+//   count: number = 0;
+
+//   handleCounter(val: string) {
+//     if (val == 'minus') {
+//       if (this.count <= 1) {
+//         this.count = 0;
+//       } else {
+//         this.count = this.count - 1;
+//       }
+//     } else if (val == 'plus') {
+//       this.count = this.count + 1;
+//     } else {
+//       this.count = 0;
+//     }
+//   }
+
+//   protected title = 'angular-tut';
+
+//   handleClickEvent() {
+//     console.log('Fuction Called');
+//     this.otherFuction();
+//   }
+
+//   otherFuction() {
+//     console.log('Other fn Called');
+//   }
+
+//   // name = 'Tushar';
+//   // data: string | number = 'Hello';
+//   other: any = true;
+
+//   updateName() {
+//     this.name = 'Sidhu';
+//     this.data = 12;
+//     this.other = 'Anil';
+//     this.other = 12;
+//   }
+
+//   handleSum(a: number, b: number) {
+//     console.log(a + b);
+//   }
+// }
+
+
+
 }
