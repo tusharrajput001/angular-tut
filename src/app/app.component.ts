945150514633c7faf8811b8465899612528e49ed
Tusharrajput007 <trajput@saffronsynaptiqsoftwarelab.com>
2025-07-05 17:54:11 +0530

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,141 +2,140 @@ import { Component, computed, effect, signal } from '@angular/core';
 import { Login } from './login/login';
 import { Signup } from './signup/signup';
 import { ProfileComponent } from './profile/profile.component';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [Login, Signup, ProfileComponent],
+  imports: [Login, Signup, ProfileComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-
-  x=signal(10);
-  y=signal(20);
-  z=computed(()=>this.x() + this.y()); 
-
-  showValue(){
-    console.log(this.z());
-    this.x.set(100)
-    console.log(this.z());
+  task = '';
+  taskList: { id: number; task: string }[] = [];
+  addTask() {
+    this.taskList.push({ id: this.taskList.length + 1, task: this.task });
+    this.task = '';
   }
-
-
-
-
-
-  data = signal< number | string >(10);
-
-  updateSignal(){
-    this.data.set("Hello");
+  deleteTask(taskId:number) {
+    this.taskList = this.taskList.filter((item) => item.id !== taskId);
   }
+  // name = 'Tushar';
+  // x=signal(10);
+  // y=signal(20);
+  // z=computed(()=>this.x() + this.y());
 
+  // showValue(){
+  //   console.log(this.z());
+  //   this.x.set(100)
+  //   console.log(this.z());
+  // }
 
+  // data = signal< number | string >(10);
 
+  // updateSignal(){
+  //   this.data.set("Hello");
+  // }
 
+  //   cnt = signal(10);
+  //   x:number = 20;
 
-//   cnt = signal(10);
-//   x:number = 20;
+  //   constructor() {
+  //     effect(() => {
+  //       console.log(this.cnt());
+  //       console.log(this.x);
+  //     });
+  //   }
 
-//   constructor() {
-//     effect(() => {
-//       console.log(this.cnt());
-//       console.log(this.x);
-//     });
-//   }
+  //   updateVal() {
+  //     this.cnt.set(this.cnt() + 1);
+  //   }
 
-//   updateVal() {
-//     this.cnt.set(this.cnt() + 1);
-//   }
+  //   updateX() {
+  //     this.x = this.x + 1;
+  //   }
 
-//   updateX() {
-//     this.x = this.x + 1;
-//   }
+  // users = ['Tushar', 'ujjwal', 'Kunal', 'Ayush', 'Mayank'];
+  // users = [];
+  //   students = [
+  //     { name: 'Tushar', age: 23, address: 'Delhi' },
+  //     { name: 'ujjwal', age: 23, address: 'UP' },
+  //     { name: 'Kunal', age: 23, address: 'Delhi' },
+  //     { name: 'Ayush', age: 23, address: 'UP' },
+  //     { name: 'Mayank', age: 23, address: 'UP' },
+  //   ];
 
-//   users = ['Tushar', 'ujjwal', 'Kunal', 'Ayush', 'Mayank'];
-//   students = [
-//     { name: 'Tushar', age: 23, address: 'Delhi' },
-//     { name: 'ujjwal', age: 23, address: 'UP' },
-//     { name: 'Kunal', age: 23, address: 'Delhi' },
-//     { name: 'Ayush', age: 23, address: 'UP' },
-//     { name: 'Mayank', age: 23, address: 'UP' },
-//   ];
+  //   color = 1;
 
-//   color = 1;
+  //   display = true;
 
-//   display = true;
+  //   handleColor(val: number) {
+  //     this.color = val;
+  //   }
 
-//   handleColor(val: number) {
-//     this.color = val;
-//   }
+  //   handleInput(event: Event) {
+  //     this.color = parseInt((event.target as HTMLInputElement).value);
+  //   }
 
-//   handleInput(event: Event) {
-//     this.color = parseInt((event.target as HTMLInputElement).value);
-//   }
+  //   name: string = '';
+  //   displayName = '';
 
-//   name: string = '';
-//   displayName = '';
+  //   getName(event: Event) {
+  //     const name = (event.target as HTMLInputElement).value;
+  //     this.name = name;
+  //   }
 
-//   getName(event: Event) {
-//     const name = (event.target as HTMLInputElement).value;
-//     this.name = name;
-//   }
+  //   showName() {
+  //     this.displayName = this.name;
+  //   }
 
-//   showName() {
-//     this.displayName = this.name;
-//   }
+  //   setName() {
+  //     this.name = 'Tushar';
+  //   }
 
-//   setName() {
-//     this.name = 'Tushar';
-//   }
+  //   toogleDiv() {
+  //     this.display = !this.display;
+  //   }
 
-//   toogleDiv() {
-//     this.display = !this.display;
-//   }
+  //   count: number = 0;
 
-//   count: number = 0;
+  //   handleCounter(val: string) {
+  //     if (val == 'minus') {
+  //       if (this.count <= 1) {
+  //         this.count = 0;
+  //       } else {
+  //         this.count = this.count - 1;
+  //       }
+  //     } else if (val == 'plus') {
+  //       this.count = this.count + 1;
+  //     } else {
+  //       this.count = 0;
+  //     }
+  //   }
 
-//   handleCounter(val: string) {
-//     if (val == 'minus') {
-//       if (this.count <= 1) {
-//         this.count = 0;
-//       } else {
-//         this.count = this.count - 1;
-//       }
-//     } else if (val == 'plus') {
-//       this.count = this.count + 1;
-//     } else {
-//       this.count = 0;
-//     }
-//   }
+  //   protected title = 'angular-tut';
 
-//   protected title = 'angular-tut';
+  //   handleClickEvent() {
+  //     console.log('Fuction Called');
+  //     this.otherFuction();
+  //   }
 
-//   handleClickEvent() {
-//     console.log('Fuction Called');
-//     this.otherFuction();
-//   }
+  //   otherFuction() {
+  //     console.log('Other fn Called');
+  //   }
 
-//   otherFuction() {
-//     console.log('Other fn Called');
-//   }
+  //   // name = 'Tushar';
+  //   // data: string | number = 'Hello';
+  //   other: any = true;
 
-//   // name = 'Tushar';
-//   // data: string | number = 'Hello';
-//   other: any = true;
+  //   updateName() {
+  //     this.name = 'Sidhu';
+  //     this.data = 12;
+  //     this.other = 'Anil';
+  //     this.other = 12;
+  //   }
 
-//   updateName() {
-//     this.name = 'Sidhu';
-//     this.data = 12;
-//     this.other = 'Anil';
-//     this.other = 12;
-//   }
-
-//   handleSum(a: number, b: number) {
-//     console.log(a + b);
-//   }
-// }
-
-
-
+  //   handleSum(a: number, b: number) {
+  //     console.log(a + b);
+  //   }
+  // }
 }
