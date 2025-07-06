53db11d9a414bfc8fe5004a973ff277ee479d6c4
Tusharrajput007 <trajput@saffronsynaptiqsoftwarelab.com>
2025-07-06 17:07:58 +0530

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,29 @@
-import { Component, computed, effect, signal } from '@angular/core';
-import { Login } from './login/login';
-import { Signup } from './signup/signup';
-import { ProfileComponent } from './profile/profile.component';
-import { FormsModule } from '@angular/forms';
-import { NgFor, NgIf } from '@angular/common';
+import { Component} from '@angular/core';
+// import { Login } from './login/login';
+// import { Signup } from './signup/signup';
+// import { ProfileComponent } from './profile/profile.component';
+// import { FormsModule } from '@angular/forms';
+// import { NgFor, NgIf } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [Login, Signup, ProfileComponent, FormsModule, NgIf, NgFor],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-
-  login = false;
-  
-
-
-
-
+  // login = false;
   // show = true;
-
   // bgColor = "red";
   // fontSize = "40px";
-
   // headingSizeBig = "80px";
   // headingSizeSmall = "30px";
-
   // zoom = true;
-
   // updateSize(){
   //   this.zoom = !this.zoom;
   // }
-
-
   // task = '';
   // taskList: { id: number; task: string }[] = [];
   // addTask() {
@@ -47,81 +37,62 @@ export class AppComponent {
   // x=signal(10);
   // y=signal(20);
   // z=computed(()=>this.x() + this.y());
-
   // showValue(){
   //   console.log(this.z());
   //   this.x.set(100)
   //   console.log(this.z());
   // }
-
   // data = signal< number | string >(10);
-
   // updateSignal(){
   //   this.data.set("Hello");
   // }
-
   //   cnt = signal(10);
   //   x:number = 20;
-
   //   constructor() {
   //     effect(() => {
   //       console.log(this.cnt());
   //       console.log(this.x);
   //     });
   //   }
-
   //   updateVal() {
   //     this.cnt.set(this.cnt() + 1);
   //   }
-
   //   updateX() {
   //     this.x = this.x + 1;
   //   }
-
   // users = ['Tushar', 'ujjwal', 'Kunal', 'Ayush', 'Mayank'];
   // users = [];
-    students = [
-      { name: 'Tushar', age: 23, address: 'Delhi' },
-      { name: 'ujjwal', age: 23, address: 'UP' },
-      { name: 'Kunal', age: 23, address: 'Delhi' },
-      { name: 'Ayush', age: 23, address: 'UP' },
-      { name: 'Mayank', age: 23, address: 'UP' },
-    ];
-
+  // students = [
+  //   { name: 'Tushar', age: 23, address: 'Delhi' },
+  //   { name: 'ujjwal', age: 23, address: 'UP' },
+  //   { name: 'Kunal', age: 23, address: 'Delhi' },
+  //   { name: 'Ayush', age: 23, address: 'UP' },
+  //   { name: 'Mayank', age: 23, address: 'UP' },
+  // ];
   //   color = 1;
-
   //   display = true;
-
   //   handleColor(val: number) {
   //     this.color = val;
   //   }
-
   //   handleInput(event: Event) {
   //     this.color = parseInt((event.target as HTMLInputElement).value);
   //   }
-
   //   name: string = '';
   //   displayName = '';
-
   //   getName(event: Event) {
   //     const name = (event.target as HTMLInputElement).value;
   //     this.name = name;
   //   }
-
   //   showName() {
   //     this.displayName = this.name;
   //   }
-
   //   setName() {
   //     this.name = 'Tushar';
   //   }
-
   //   toogleDiv() {
   //     this.display = !this.display;
   //   }
-
   //   count: number = 0;
-
   //   handleCounter(val: string) {
   //     if (val == 'minus') {
   //       if (this.count <= 1) {
@@ -135,29 +106,23 @@ export class AppComponent {
   //       this.count = 0;
   //     }
   //   }
-
   //   protected title = 'angular-tut';
-
   //   handleClickEvent() {
   //     console.log('Fuction Called');
   //     this.otherFuction();
   //   }
-
   //   otherFuction() {
   //     console.log('Other fn Called');
   //   }
-
   //   // name = 'Tushar';
   //   // data: string | number = 'Hello';
   //   other: any = true;
-
   //   updateName() {
   //     this.name = 'Sidhu';
   //     this.data = 12;
   //     this.other = 'Anil';
   //     this.other = 12;
   //   }
-
   //   handleSum(a: number, b: number) {
   //     console.log(a + b);
   //   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Login } from './login/login';
+import { Contactus } from './contactus/contactus';
+import { Home } from './home/home';
+import { PageNotFound } from './page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'about',component: About},
+    {path:'login',component: Login},
+    {path:'contact',component: Contactus},
+    {path:'',component: Home},
+    {path:'**',component: PageNotFound},
+    
+];
